2e7748a2d4e53c04736067de781fbb31ffa6ac8d
S <S@DESKTOP-C3VEVC1>
2023-12-13 09:17:01 +0900

    초기 커밋.

---
+++ studyProject/Class1.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace studyProject
+{
+    class Class1
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            
+            int a = 5;
+            int b = 5;
+            a = 10;
+            int sum = a + b;
+            Console.WriteLine(sum);
+            
+            int x, y, z;
+            x = 2;
+            y = 2;
+            z = 2;
+            int mul = x * y * z;
+            Console.WriteLine(mul);
+            Console.WriteLine("{0},{1},{2}", x, y, z);
+        }
+    }
+}
