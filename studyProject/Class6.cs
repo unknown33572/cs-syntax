c2807638b301a6967603958c21936e881075a529
S <unknown33572@gmail.com>
2023-12-19 09:19:43 +0900

    array

---
+++ studyProject/Class6.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace studyProject
+{
+    class Class6
+    {
+        static void Main()
+        {
+            /*
+            int goal = 22;
+            int sum = 0;
+
+            int i = 1;
+            while(i <= 10)
+            {
+                sum += i;
+
+                if(sum >= goal)
+                {
+                    break;
+                }
+                i++;
+            }
+            Console.WriteLine($"1부터 {i}까지의 합은 {sum}이고, 목표치 {goal} 이상을 달성했습니다.");
+            */
+
+            Console.WriteLine("Start");
+            Start:
+            Console.WriteLine("0,1,2 중 하나 입력 : _\b");
+            int chapter = int.Parse(Console.ReadLine());
+
+            if (chapter == 1)
+            {
+                goto Chapter1;
+            }
+            else if(chapter == 2)
+            {
+                goto Chapter2;
+            }
+            else
+            {
+                goto End;
+            }
+        Chapter1:
+            Console.WriteLine("1장");
+            goto Start;
+        Chapter2:
+            Console.WriteLine("2장");
+            goto Start;
+        End:
+            Console.WriteLine("종료");
+        }
+    }
+}
---
+++ studyProject/Class7.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace studyProject
+{
+    class Class7
+    {
+        static void Main()
+        {
+            int[] subject = new int[3];
+            int sum = 0;
+            float avg = 0;
+
+            Console.WriteLine("과목 점수를 차례대로 입력하시오. 국어 -> 수학 -> 영어");
+            subject[0] = int.Parse(Console.ReadLine());
+            subject[1] = int.Parse(Console.ReadLine()); ;
+            subject[2] = int.Parse(Console.ReadLine()); ;
+
+            sum = subject[0] + subject[1] + subject[2];
+            avg = sum / (float)subject.Length;
+
+            Console.WriteLine($"총점 : {sum}, 평균 : {avg:.0}");
+        }
+    }
+}
