6ca6fb41006f6bc074df613753d8ac62c8c01d20
S <unknown33572@gmail.com>
2023-12-20 15:53:44 +0900

    C#_API and Struct_Time Method

--- studyProject/Class14.cs
+++ studyProject/Class14.cs
@@ -8,12 +8,21 @@ namespace studyProject
 {
     class Class14
     {
+        struct Point
+        {
+            public int X;
+            public int Y;
+        }
+
         static void Main(string[] args)
         {
-            foreach(string arg in args)
-            {
-                Console.WriteLine(arg);
-            }
+            Point p;
+            p.X = 100;
+            p.Y = 200;
+            Console.WriteLine(p.X + p.Y);
+
+            TimeSpan d_day = /*Convert.ToDateTime("2023-12-25")*/new DateTime(2023,12,25) - DateTime.Now;
+            Console.WriteLine($"{DateTime.Now.Year}년도 크리스마스는 {(int)d_day.TotalDays}일 남음");
         }
     }
 }
