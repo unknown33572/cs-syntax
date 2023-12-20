46c48111085095b83c1df196d7aed0205f2a8788
S <unknown33572@gmail.com>
2023-12-20 15:23:32 +0900

    args

---
+++ studyProject/Class14.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace studyProject
+{
+    class Class14
+    {
+        static void Main(string[] args)
+        {
+            foreach(string arg in args)
+            {
+                Console.WriteLine(arg);
+            }
+        }
+    }
+}
