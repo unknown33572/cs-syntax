b1224a3393ecaf1459f1b2a62e93f9582566d92c
S <unknown33572@gmail.com>
2023-12-20 17:09:05 +0900

    enum

---
+++ studyProject/Class15.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace studyProject
+{
+    enum Priorty
+    {
+        High,
+        Normal,
+        Low
+    }
+
+    class Class15
+    {
+        static void Main()
+        {
+            Priorty high = Priorty.High;
+            Priorty normal = Priorty.Normal;
+            Priorty low = Priorty.Low;
+
+            Console.WriteLine($"{high},{normal},{low}");
+        }
+    }
+}
