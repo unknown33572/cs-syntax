82acf4fecd664103a74e2bc30128cc409c953fea
S <unknown33572@gmail.com>
2023-12-27 18:02:54 +0900

    lamda

---
+++ studyProject/Class24.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Collections;
+
+namespace studyProject
+{
+    class Class24
+    {
+        static void Main()
+        {
+            var numbers = Enumerable.Range(1, 10);
+            Console.WriteLine(numbers);
+
+            foreach (var n in numbers)
+                Console.WriteLine("{0}\t", n);
+        }
+    }
+}
---
+++ studyProject/Class25.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace studyProject
+{
+    class Class25
+    {
+        static void Main()
+        {
+            int[] numbers = { 1, 2, 3, 4, 5 };
+            List<int> newNumbers = numbers.Where(n => n > 3).ToList();
+            for(int i = 0; i < newNumbers.Count; i++)
+            {
+                Console.WriteLine(newNumbers[i]);
+            }
+        }
+    }
+}
