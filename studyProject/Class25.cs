c56a4076a6572eea568aec91d154e4993e84ebb2
S <unknown33572@gmail.com>
2023-12-28 10:24:12 +0900

    modeAlgorithm

--- studyProject/Class25.cs
+++ studyProject/Class25.cs
@@ -16,6 +16,12 @@ namespace studyProject
             {
                 Console.WriteLine(newNumbers[i]);
             }
+
+            int[] arr = new int[5];
+            for(int i = 0; i < arr.Length; i++)
+            {
+                arr[i] = i + 1;
+            }
         }
     }
 }
