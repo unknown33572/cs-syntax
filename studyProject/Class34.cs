5d850abc9e8781fa0d070c06171c926eda70d2fe
S <unknown33572@gmail.com>
2024-01-04 15:44:25 +0900

    delegate

--- studyProject/Class34.cs
+++ studyProject/Class34.cs
@@ -17,6 +17,34 @@ namespace studyProject
             set { names[key] = value; }
         }
 
+
+        static IEnumerable<int> GreaterOne(int[] numbers, int greater)
+        {
+            List<int> temp = new List<int>();
+
+            foreach(var n in numbers)
+            {
+                if(n > greater)
+                {
+                    temp.Add(n);
+                }
+            }
+
+            return temp;
+        }
+
+        static IEnumerable<int> GreaterTwo(int[] numbers, int greater)
+        {
+            foreach(var n in numbers)
+            {
+                if(n > greater)
+                {
+                    yield return n;
+                }
+            }
+        }
+
+
         static void Main()
         {
             var nickName = new Class34();
@@ -25,6 +53,19 @@ namespace studyProject
             nickName["이순신"] = "장군";
 
             Console.WriteLine($"{nickName["홍길동"]},{nickName["이순신"]}");
+
+            int[] numbers = { 1, 2, 3, 4, 5 };
+            foreach(var n in GreaterOne(numbers, 2))
+            {
+                Console.WriteLine(n);
+            }
+
+            Console.WriteLine();
+
+            foreach(var n in GreaterTwo(numbers, 3))
+            {
+                Console.WriteLine(n);
+            }
         }
     }
 }
