db79db84750044d8d71995fb4319ee220f2937dc
unknown33572 <unknown33572@gmail.com>
2024-01-05 19:23:30 +0900

    InsertionSort

--- studyProject/Class36.cs
+++ studyProject/Class36.cs
@@ -8,6 +8,10 @@ namespace studyProject
 {
   class Class36
   {
+    static Action<string> printfCustom = Console.WriteLine;
+
+    static Func<int, int> absCustom() => Math.Abs;
+
     static void Main()
 		{
       int x = 1;
@@ -15,6 +19,31 @@ namespace studyProject
       int z = x + y;
 
       Console.WriteLine(z);
+
+      printfCustom(z.ToString());
+
+      Console.WriteLine(absCustom()(-100));
+
+      int[] arr = { 2, 4, 1, 7, 12, 15 };
+      InsertionSort(arr);
+      Console.WriteLine(String.Join(", ", arr));
+		}
+
+    static void InsertionSort(int[] arr)
+		{
+      for(int i = 1; i < arr.Length; i++)
+			{
+        int key = arr[i];
+        int j = i - 1;
+
+        while(j >= 0 && arr[j] > key)
+				{
+          arr[j + 1] = arr[j];
+          j = j - 1;
+				}
+
+        arr[j + 1] = key;
+			}
 		}
   }
 }
