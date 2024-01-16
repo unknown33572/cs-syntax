c124570a5e993215f145fde312ef33ba4c0623ba
unknown33572 <unknown33572@gmail.com>
2024-01-16 13:42:20 +0900

    abstract class, polymorphism

---
+++ studyProject/Class46.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace studyProject
+{
+    class Class46
+    {
+        private string name;
+
+        public void setName(string n) => name = n;
+        public string GetName() => this.name;
+    }
+
+    class ReflectionGetProperty
+    {
+        static void Main()
+        {
+            Class46 cs46 = new Class46();
+            cs46.setName("C#");
+            Console.WriteLine(cs46.GetName());
+        }
+    }
+}
---
+++ studyProject/Class47.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace studyProject
+{
+    public abstract class Animal
+    {
+        public abstract string Cry();
+    }
+
+    public class Dog : Animal
+    {
+        public override string Cry()
+        {
+            return "멍멍";
+        }
+    }
+
+    public class Cat : Animal
+    {
+        public override string Cry()
+        {
+            return "야옹";
+        }
+    }
+
+    public class Trainer
+    {
+        public void DoCry(Animal animal) // 상속받은 객체 파라미터로 받음
+        {
+            Console.WriteLine("{0}", animal.Cry()); // Animal Cry 메서드
+        }
+    }
+
+    class DemoTest
+    {
+        static void Main()
+        {
+            Animal dog = new Dog();
+            Animal cat = new Cat();
+            Console.WriteLine(dog.Cry());
+            Console.WriteLine(cat.Cry());
+
+            Trainer trainer = new Trainer();
+            trainer.DoCry(new Dog());
+            trainer.DoCry(new Cat());
+        }
+    }
+}
