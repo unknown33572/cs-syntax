7969c790e83ea7f0af8c693a6e5c8f799bab2f00
unknown33572 <unknown33572@gmail.com>
2024-01-17 18:11:26 +0900

    generic

---
+++ studyProject/Class49.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace studyProject
+{
+    class Class49
+    {
+        public string name { get; set; }
+
+        static void Main()
+        {
+            List<Class49> people = new List<Class49>
+            {
+                new Class49 { name = "홍길동" },
+                new Class49 { name = "이순신" },
+                new Class49 { name = "유관순" }
+            };
+
+            foreach(var person in people)
+            {
+                Console.WriteLine(person.name);
+            }
+        }
+    }
+}
