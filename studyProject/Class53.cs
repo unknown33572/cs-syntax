fabd9386bb55a65c2b9d2a25fd3fbad40a7c3c0b
unknown33572 <unknown33572@gmail.com>
2024-01-19 17:49:38 +0900

    asynchronous

---
+++ studyProject/Class53.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace studyProject
+{
+    class Class53
+    {
+
+    }
+
+    class SignBase
+    {
+        public int SignID { get; set; }
+        private string password;
+        private String email;
+
+        public string Password
+        {
+            get { return password; }
+            set { this.password = value; }
+        }
+
+        public void setEmail(String email)
+        {
+            this.email = email;
+        }
+
+        public String getEmail()
+        {
+            return this.email;
+        }
+    }
+
+    class SignRepository
+    {
+        public List<SignBase> GetAll()
+        {
+            new SignBase() { SignID = 1}
+        }
+    }
+}
---
+++ studyProject/JavaScript1.js
@@ -0,0 +1,35 @@
+﻿function makeRice() {
+  return new Promise((resolve) => {
+    setTimeout(() => {
+      console.log("밥이 완성되었습니다.")
+      resolve();
+    }, 1000);
+  });
+}
+
+function makeSoup() {
+  return new Promise((resolve) => {
+    setTimeout(() => {
+      console.log("국이 완성되었습니다.")
+      resolve();
+    }, 1000);
+  });
+}
+
+function makeEgg() {
+  return new Promise((resolve) => {
+    setTimeout(() => {
+      console.log("후라이가 완성되었습니다.")
+    }, 1000);
+    resolve();
+  }).then(() => {
+    console.log("게임중");
+  });
+}
+
+async function cookMeal() {
+  await Promise.all([makeRice(), makeSoup(), makeEgg()]);
+  console.log("모든 요리가 완성되었습니다.");
+}
+
+cookMeal();
