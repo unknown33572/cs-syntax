1982048a4f0a0c3989de6327da3286e14fbf20d0
unknown33572 <unknown33572@gmail.com>
2024-01-25 11:25:13 +0900

    프로그래머스

---
+++ studyProject/JavaScript2.js
@@ -0,0 +1,8 @@
+﻿function solution(my_strings, parts) {
+  var answer = '';
+  console.log(my_strings.length);
+  console.log(parts);
+  console.log(parts[0]);
+}
+
+solution(["pro", "gram", "mers"], [[0,2],[0,3],[0,3]]);
