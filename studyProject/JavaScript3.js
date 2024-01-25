4c0a5f47506e252a95fd189a7058eac7186dab29
unknown33572 <unknown33572@gmail.com>
2024-01-25 13:48:35 +0900

    algorithm

---
+++ studyProject/JavaScript3.js
@@ -0,0 +1,18 @@
+﻿function solution(arr, delete_list) {
+  var answer = [];
+  for (let i = 0; i < arr.length; i++) {
+    let flag = false;
+    for (let j = 0; j < delete_list.length; j++) {
+      if (arr[i] === delete_list[j]) {
+        flag = true;
+        break;
+      }
+    }
+    if (!flag) {
+      answer.push(arr[i]);
+    }
+  }
+  return answer;
+}
+
+console.log(solution([123, 456, 789], [123, 789]));
