a46f0bd0c33670cbc19f811fd98dc670c7eaf3d4
unknown33572 <unknown33572@gmail.com>
2024-01-31 09:11:54 +0900

    code

---
+++ studyProject/JavaScript9.js
@@ -0,0 +1,12 @@
+﻿function solution(arr, idx) {
+  var answer = 0;
+  for (idx; idx < arr.length; idx++) {
+    if (arr[idx] === 1) {
+      answer = idx;
+      break;
+    } else {
+      answer = -1;
+    }
+  }
+  return answer;
+}
