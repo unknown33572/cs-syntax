d8c66fb1f92d5613bbdd42c10247e8efe84b06a5
unknown33572 <unknown33572@gmail.com>
2024-02-05 13:42:59 +0900

    Algorithm

---
+++ studyProject/js/JavaScript12.js
@@ -0,0 +1,15 @@
+﻿function solution(str_list, ex) {
+  let answer = '';
+  let result = '';
+  let arr = [];
+  for (let x of str_list) {
+    if (!x.includes(ex)) {
+      arr.push(x);
+    }
+    result = arr.join(',')
+  }
+  answer = result.split(',').join('');
+  return answer;
+}
+
+console.log(solution(['longlong', 'long', 'longlonglong', 'longlonglonglong'], 'long'));
