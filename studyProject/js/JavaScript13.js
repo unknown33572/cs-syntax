2afe581e173658e667769559429235e4385c80d5
unknown33572 <unknown33572@gmail.com>
2024-02-06 13:41:04 +0900

    Algorithm

---
+++ studyProject/js/JavaScript13.js
@@ -0,0 +1,23 @@
+﻿function solution(myString) {
+  let answer = '';
+  let regex = /[a-k]/g;
+  if (myString.match(regex)) {
+    answer = myString.replace(/[a-k]/g, 'l');
+  }
+  return answer;
+}
+
+/*
+function solution(myString) {
+  return [...myString].reduce((acc, cur) => {
+    if (cur < 'l') {
+      acc += 'l';
+    }
+    else {
+      acc += cur;
+    }
+
+    return acc;
+  }, '');
+}
+*/
---
+++ studyProject/js/JavaScript14.js
@@ -0,0 +1,19 @@
+﻿function solution(myString) {
+  var answer = [];
+  answer = myString.split('x');
+  for(var i=0; i<answer.length; i++) {
+    answer[i] = answer[i].length;
+  }
+  return answer;
+}
+
+console.log(solution('oxooxoxxox'));
+
+function solution(myString) {
+  return myString.split('x').map(v => v.length);
+}
+
+const solution = (myString) => {
+  const arr = myString.split('x');
+  return arr.reduce((a, c) => [...a, c.length], []);
+}
