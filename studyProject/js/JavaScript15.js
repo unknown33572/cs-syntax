6f347bbcd7b9f42ae7d11dcce5f89bf593a4e934
unknown33572 <unknown33572@gmail.com>
2024-02-06 16:53:48 +0900

    Algorithm

---
+++ studyProject/js/JavaScript15.js
@@ -0,0 +1,16 @@
+﻿function solution(strArr) {
+  var answer = [];
+
+  for (var i = 0; i < strArr.length; i++) {
+    if (!strArr[i].includes('ad')) {
+      answer.push(strArr[i]);
+    }
+  }
+
+  return answer;
+}
+
+function solution(strArr) {
+  var answer = [];
+  answer = strArr.filter(v => !v.includes('ad'));
+}
---
+++ studyProject/js/JavaScript16.js
@@ -0,0 +1,22 @@
+﻿function solution(my_string) {
+  var answer = [];
+  var len = my_string.length;
+  for (var i = len; i > 0; i--) {
+      let substring = my_string.slice(i - 1);
+      answer.push(substring);
+  }
+  answer.sort();
+  return answer;
+}
+
+console.log(solution('banana'));
+
+function solution(my_string) {
+  return [...my_string].reduce((acc, cur, idx, arr) => {
+    const original = arr.join('');
+
+    acc = [...acc, original.slice(idx)].sort();
+
+    return acc;
+  }, []);
+}
