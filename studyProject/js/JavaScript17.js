64a06dfd9b9c700ad67dd76915b3c4ab2c28e6b4
unknown33572 <unknown33572@gmail.com>
2024-02-06 17:55:02 +0900

    ALGORITHM

---
+++ studyProject/js/JavaScript17.js
@@ -0,0 +1,28 @@
+﻿function solution(binomial) {
+  var answer = 0;
+  var arr = binomial.split(' ');
+  if (arr[1] === '+') {
+    answer = parseInt(arr[0]) + parseInt(arr[2]);
+  }
+  if (arr[1] === '-') {
+      answer = parseInt(arr[0]) - parseInt(arr[2]);
+  }
+  if(arr[1] === '*') {
+    answer = parseInt(arr[0]) * parseInt(arr[2]);
+  }
+  if(arr[1] === '/') {
+      answer = parseInt(arr[0]) / parseInt(arr[2]);
+  }
+  return answer;
+}
+
+const ops = {
+  '+': (a, b) => a + b,
+  '-': (a, b) => a - b,
+  '*': (a, b) => a * b,
+};
+
+function solution(binomial) {
+  const [a, op, b] = binomial.split(' ');
+  return ops[op](+a, +b);
+}
