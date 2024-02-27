029836dfca7a0903c33d38b4f9f237aef5d06b78
unknown33572 <unknown33572@gmail.com>
2024-02-27 17:20:27 +0900

    algorithm

---
+++ studyProject/js/JavaScript23.js
@@ -0,0 +1,19 @@
+﻿function solution(my_string, indices) {
+  var answer = '';
+  for (var i = 0; i < my_string.length; i++) {
+    if (indices.indexOf(i) === -1) {
+      answer += my_string[i];
+    }
+  }
+  return answer;
+}
+
+function solutionTwo(my_string, indices) {
+  const stringArr = my_string.split('');
+  const filteredArr = stringArr.filter((x, index) => !indices.includes(index));
+
+  return filteredArr.join('');
+}
+
+console.log(solution('apporoograpemmemprs', [1, 16, 6, 15, 0, 10, 11, 3]));
+console.log(solutionTwo('apporoograpemmemprs', [1, 16, 6, 15, 0, 10, 11, 3]));
---
+++ studyProject/js/JavaScript24.js
@@ -0,0 +1,9 @@
+﻿function solution(my_string, s, e) {
+  var answer = '';
+  stringArr = my_string.split('');
+  var temp = stringArr.slice(s, e + 1).reverse();
+  for (var i = s; i <= e; i++) {
+    stringArr[i] = temp[i - e];
+  }
+  return answer = stringArr.join('');
+}
