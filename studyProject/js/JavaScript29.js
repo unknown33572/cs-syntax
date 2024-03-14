903dd0e94f075b3fbe73c265150b28f2abdd0794
unknown33572 <unknown33572@gmail.com>
2024-03-14 19:20:21 +0900

    javascript arrow func

--- studyProject/js/JavaScript29.js
+++ studyProject/js/JavaScript29.js
@@ -1,1 +1,38 @@
-﻿
+﻿const UC = {
+}
+
+const UC2 = {
+  images: []
+}
+
+const UC3 = {
+  images: [
+    'image1.jpg',
+    'image2.jpg',
+  ]
+}
+
+//const imgCheck = UC.images[0] || 'default.jpg';
+
+//console.log(imgCheck);
+
+function getFirstImage(uc) {
+  let img = 'default.jpg';
+  if (UC.images) {
+    img = UC.images[0];
+  }
+
+  return img;
+}
+
+console.log(getFirstImage(UC2));
+
+function exampleCallback(callback) {
+  // 무언가 작업 수행
+  console.log("작업 완료");
+  callback();
+}
+
+exampleCallback(function () {
+  console.log("콜백 함수 실행됨");
+});
---
+++ studyProject/js/JavaScript30.js
@@ -0,0 +1,11 @@
+﻿const capitalize = name => name[0].toUpperCase() + name.slice(1);
+
+function greet(name) {
+  return `Hello, ${name}`;
+}
+
+function applyCustomGreeting(name, callback) {
+  return callback(capitalize(name));
+}
+
+console.log(applyCustomGreeting('mark', greet)); // applyCustomGreeting -> greet -> capitalize
