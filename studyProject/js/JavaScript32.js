73233872276e029b80017625ca3cbfb8624df57f
unknown33572 <unknown33572@gmail.com>
2024-03-20 10:52:56 +0900

    js

---
+++ studyProject/js/JavaScript32.js
@@ -0,0 +1,46 @@
+﻿//const instructors = [
+//  { name: 'Quincy', libraries: ['Quantum Mechanics'] },
+//  { name: 'Kenn', libraries: ['Norse Mythology'] },
+//  { name: 'Paul', libraries: ['Tuvan throat singing'] },
+//  { name: 'Aaron', libraries: ['Entomology', 'Kung Fu'] },
+//];
+
+//const findInstructor = instructors.find(instructor => {
+//  return instructor.libraries.includes('Entomology');
+//});
+
+//console.log(findInstructor);
+
+//const findByLibrary = library => instructors => {
+//  return instructors.libraries.includes(library);
+//};
+
+//const librarian = instructors.find(findByLibrary('Kung Fu'));
+
+//console.log(librarian);
+
+const sailors = [
+  {
+    name: 'Jim',
+    active: true,
+    email: 'jim@gmail.com'
+  },
+  {
+    name: 'Sally',
+    active: true,
+    email: 'sally@naver.com'
+  },
+  {
+    name: 'Tom',
+    active: false,
+    email: ''
+  }
+];
+
+const activeSailors = sailors.filter(sailor => sailor.active);
+
+//console.log(activeSailors);
+
+const emails = activeSailors.filter(sailor => sailor.email && sailor.email.includes('@gmail.com'));
+
+console.log(emails);
---
+++ studyProject/js/JavaScript33.js
@@ -0,0 +1,45 @@
+﻿//const callback = function (collectedValues, item) {
+//  return [...collectedValues, item];
+//};
+
+//const saying = ['veni', 'vedi', 'veci'];
+
+//const initialValue = [];
+
+//const result = saying.reduce(callback, initialValue, 2);
+
+//console.log(result);
+
+//const numbers = [1, 2, 3, 4, 5];
+//const sum = numbers.reduce((accumulator, currentValue) => accumulator + currentValue , 10);
+//console.log(sum);
+
+const dogs = [
+  {
+    이름: '맥스',
+    크기: '소형견',
+    견종: '보스턴 테리어',
+    색상: '검정색'
+  },
+  {
+    이름: '도니',
+    크기: '대형견',
+    견종: '래브라도레트리버',
+    색상: '검정색'
+  },
+  {
+    이름: '섀도',
+    크기: '중형견',
+    견종: '래브라도레트리버',
+    색상: '갈색'
+  }
+];
+
+const colors = dogs.reduce((colors, dog) => {
+  if (colors.includes(dog['색상'])) {
+    return colors;
+  }
+  return [...colors, dog['색상']];
+}, []);
+
+console.log(colors);
