355c19213399896c5c4568b428a72b1a07815402
unknown33572 <unknown33572@gmail.com>
2024-03-20 18:05:31 +0900

    js

--- studyProject/js/JavaScript35.js
+++ studyProject/js/JavaScript35.js
@@ -5,6 +5,49 @@
 
 //console.log(validateCharacterCount(10, 'wvoquie'));
 
-const discounter = d => p => p * (1 - d);
-const tenPercentOff = discounter(0.1);
-console.log(discounter(0.1)(100));
+//const discounter = d => p => p * (1 - d);
+//const tenPercentOff = discounter(0.1);
+//console.log(discounter(0.1)(100));
+
+function mergeProgramInfo(building, manager) {
+  const { hours, address } = building;
+  const { name, phone } = manager;
+  const defaults = {
+    hours,
+    address,
+    contact: name,
+    phone
+  };
+
+  return x => { // 두 번째 함수 커링 처리
+    return { ...defaults, ...x };
+  }
+}
+
+const building = {
+  hours: '8 a.m. - 8 p.m.',
+  address: 'Jayhawk Blvd'
+};
+
+const manager = {
+  name: 'Augusto',
+  phone: '555-5555'
+};
+
+const program = {
+  name: 'Presenting Research',
+  room: '415',
+  hours: '3 - 6 p.m.'
+};
+
+const exhibit = {
+  name: 'Emerging Scholarship',
+  contact: 'Dana'
+}
+
+const exhibitInfo = mergeProgramInfo(building, manager)(exhibit);
+
+const programInfo = mergeProgramInfo(building, manager)(program);
+
+console.log(programInfo);
+console.log(exhibitInfo);
