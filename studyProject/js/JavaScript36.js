bb77f130c18c77123af6ffabd6096b3b9f8f6a28
unknown33572 <unknown33572@gmail.com>
2024-03-21 18:12:24 +0900

    js

---
+++ studyProject/js/JavaScript36.js
@@ -0,0 +1,29 @@
+﻿const dogs = [
+  {
+    name: 'max',
+    weight: '20',
+    breeds: 'boston terrier',
+  },
+  {
+    name: 'don',
+    weight: '50',
+    breeds: 'labrador',
+  },
+  {
+    name: 'shadow',
+    weight: '10',
+    breeds: 'maltese',
+  },
+];
+
+function getDogNames(dogs, func) {
+  //const [key, value] = filter;
+  //return dogs.filter(dog => dog[key] <= value).map(dog => dog.name);
+  return dogs.filter(func).map(dog => dog.name);
+}
+
+//console.log(getDogNames(dogs, ['weight', '20']));
+
+const weightCheck = weight => dog => dog['weight'] >= weight;
+
+console.log(getDogNames(dogs, weightCheck(50)));
