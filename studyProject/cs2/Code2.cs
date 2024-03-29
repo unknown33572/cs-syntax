6f1adef40e9b10d9cd9594e0084e0f47154d83f7
unknown33572 <unknown33572@gmail.com>
2024-03-29 09:08:46 +0900

    code

--- studyProject/cs2/Code2.cs
+++ studyProject/cs2/Code2.cs
@@ -7,7 +7,7 @@ using System.Threading.Tasks;
 
 namespace studyProject.cs2
 {
-    class Class1
+    class Code2
     {
         static void Main(string[] args)
         {
@@ -18,32 +18,10 @@ namespace studyProject.cs2
             twoDimensionalList.Add(new List<int> { 4, 5, 6 });
             twoDimensionalList.Add(new List<int> { 7, 2, 3 });
 
-            //Console.WriteLine(twoDimensionalList[0][0]); // 1
-
-            //if (twoDimensionalList[0][0].Equals(twoDimensionalList[2][0]))
-            //{
-            //    Console.WriteLine(twoDimensionalList[0][0] + "있음");
-            //}
-
-            //int count = 0;
-
             for (int i = 0; i < 3; i++)
             {
                 for (int j = i + 1; j < 3; j++)
                 {
-                    //if(twoDimensionalList[i][0].Equals(twoDimensionalList[j][0]))
-                    //{
-                    //    Console.WriteLine(twoDimensionalList[i][0] + "있음");
-                    //}
-                    //if (twoDimensionalList[i][1].Equals(twoDimensionalList[j][1]))
-                    //{
-                    //    Console.WriteLine(twoDimensionalList[i][1] + "있음");
-                    //}
-                    //if (twoDimensionalList[i][2].Equals(twoDimensionalList[j][2]))
-                    //{
-                    //    Console.WriteLine(twoDimensionalList[i][2] + "있음");
-                    //}
-
                     for(int k = 0; k < 3; k++)
                     {
                         if (twoDimensionalList[i][k].Equals(twoDimensionalList[j][k]))
@@ -53,6 +31,22 @@ namespace studyProject.cs2
                     }
                 }
             }
+
+            String[] input;
+            Console.Clear();
+            input = Console.ReadLine().Split(' ');
+
+            String s1 = input[0];
+            int a = Int32.Parse(input[1]);
+
+            String answer = "";
+
+            for(int i = 0; i < a; i++)
+            {
+                answer += s1;
+            }
+            
+            Console.WriteLine(answer);
         }
     }
 }
