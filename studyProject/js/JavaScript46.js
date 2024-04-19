25ebdfc2bc9a30346665be2e2945fc9b482c48a8
unknown33572 <unknown33572@gmail.com>
2024-04-19 17:58:50 +0900

    LinearScanCloser

---
+++ studyProject/js/JavaScript46.js
@@ -0,0 +1,26 @@
+﻿function linearScanClosestNeighbor(arr, targer, dist) {
+  let N = arr.length;
+  if (N === 0) {
+    return null;
+  }
+
+  let candidate = arr[0];
+  let closestDistance = dist(target, candidate);
+
+  for (let i = 1; i < N; i++) {
+    let currentDistance = dist(target, arr[i]);
+    if(currentDistance < closestDistance) {
+      candidate = arr[i];
+      closestDistance = currentDistance;
+    }
+  }
+  return candidate;
+}
+
+function dist(a, b) {
+  return Math.abs(a - b);
+}
+
+let arr = [1, 2, 3, 4, 5];
+
+console.log(linearScanClosestNeighbor(arr, 3.5, dist)); // 4
