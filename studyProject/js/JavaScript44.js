5fa257d9a8972b1fd7193ab8628182a2839623d5
unknown33572 <unknown33572@gmail.com>
2024-04-19 16:03:00 +0900

    hashtable

--- studyProject/js/JavaScript44.js
+++ studyProject/js/JavaScript44.js
@@ -24,4 +24,22 @@
       current = current.children[ch];
     }
   }
+
+  remove() {
+    if (this.heap.length <= 1) return null;
+    if (this.heap.length === 2) return this.heap.pop();
+    const max = this.heap[1];
+    this.heap[1] = this.heap.pop();
+    let currentIndex = 1;
+    let [leftIndex, rightIndex] = [2 * currentIndex, 2 * currentIndex + 1];
+    let maxChildIndex = this.heap[rightIndex] && this.heap[rightIndex] > this.heap[leftIndex] ? rightIndex : leftIndex;
+
+    while (this.heap[maxChildIndex] && this.heap[currentIndex] < this.heap[maxChildIndex]) {
+      [this.heap[currentIndex], this.heap[maxChildIndex]] = [this.heap[maxChildIndex], this.heap[currentIndex]];
+      currentIndex = maxChildIndex;
+      [leftIndex, rightIndex] = [2 * currentIndex, 2 * currentIndex + 1];
+      maxChildIndex = this.heap[rightIndex] && this.heap[rightIndex] > this.heap[leftIndex] ? rightIndex : leftIndex;
+    }
+    return max;
+  }
 }
---
+++ studyProject/js/JavaScript45.js
@@ -0,0 +1,75 @@
+﻿function hashTable(size) {
+  this.size = size;
+  this.bins = new Array(size).fill(null);
+}
+
+function ListNode(key, value) {
+  this.key = key;
+  this.value = value;
+  this.next = null;
+}
+
+hashTable.prototype.hashFunction = function(key) {
+  return key % this.size;
+};
+
+hashTable.prototype.insert = function(key, value) {
+  const index = this.hashFunction(key);
+  let current = this.bins[index];
+
+  if(!current) {
+    this.bins[index] = new ListNode(key, value);
+    return;
+  }
+
+  while(current.next && current.key !== key) {
+    current = current.next;
+  }
+
+  if(current.key === key) {
+    current.value = value;
+  } else {
+    current.next = new ListNode(key, value);
+  }
+};
+
+hashTable.prototype.lookup = function(key) {
+  const index = this.hashFunction(key);
+  let current = this.bins[index];
+
+  while (current) {
+    if (current.key === key) {
+      return current.value;
+    }
+    current = current.next;
+  }
+  return null;
+};
+
+hashTable.prototype.remove = function(key) {
+  const index = this.hashFunction(key);
+  let current = this.bins[index];
+  let prev = null;
+
+  while (current) {
+    if (current.key == key) {
+      if(prev) {
+        prev.next = current.next;
+      } else {
+        this.bins[index] = current.next;
+      }
+      return true;
+    }
+  }
+  return false;
+};
+
+var ht = new hashTable(10);
+
+ht.insert(0, 'zero');
+ht.insert(1, 'one');
+ht.insert(2, 'two');
+ht.insert(3, 'three');
+ht.insert(10, 'ten');
+
+console.log(ht.bins);
