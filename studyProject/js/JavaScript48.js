1d37ae67417b65fd6b86b4d876f2408fade31c7a
dishin <dishin@maitzsoft.co.kr>
2024-05-16 09:59:23 +0900

    codingtest

---
+++ studyProject/js/JavaScript48.js
@@ -0,0 +1,34 @@
+﻿function solution(n) {
+  var answer = [[]];
+  for (let i = 0; i < n; i++) {
+    answer[i] = [];
+    for (let j = 0; j < n; j++) {
+      if (i == j) {
+        answer[i][j] = 1;
+      } else {
+        answer[i][j] = 0;
+      }
+    }
+  }
+  return answer;
+}
+
+//console.log(solution(3));
+
+function solution2(arr) {
+  var answer = 0;
+  for (let i = 0; i < arr.length; i++) {
+    for (let j = 0; j < arr[i].length; j++) {
+      if (arr[i][j] == arr[j][i]) {
+        answer = 1;
+      } else {
+        answer = 0;
+        break;
+      }
+    }
+  }
+  return answer;
+}
+
+var x = solution2([[5, 192, 33], [192, 72, 95], [33, 95, 999]]);
+
