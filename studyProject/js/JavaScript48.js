feb6eb17facc26fa3f2e24efb90ed7497841b0c5
dishin <dishin@maitzsoft.co.kr>
2024-05-20 17:10:57 +0900

    coding

--- studyProject/js/JavaScript48.js
+++ studyProject/js/JavaScript48.js
@@ -38,7 +38,7 @@ function solution3(board, k) {
     for (let j = 0; j < board[i].length; j++) {
       if (i + j <= k) {
         answer += board[i][j];
-        console.log(board[i][j]);
+        //console.log(board[i][j]);
       }
     }
   }
@@ -54,21 +54,72 @@ var x = solution3(
 
 //console.log(x);
 
-function solution4(arr) {
-  var answer = [];
-  for (let i = 0; i < arr.length; i++) {
-    if (arr[i] == 2) {
-      for(let j = i; j < arr.length; j++) {
-        if (arr[j + 1] != 2) {
-          answer.push(j);
-        }
-        else {
-          break;
-        }
-      }
+function mergeSort(arr) {
+  if (arr.length <= 1) return arr;
+
+  const mid = Math.floor(arr.length / 2);
+  const left = mergeSort(arr.slice(0, mid));
+  const right = mergeSort(arr.slice(mid));
+
+  return merge(left, right);
+}
+
+function merge(left, right) {
+  let result = [];
+  let leftIndex = 0;
+  let rightIndex = 0;
+
+  while (leftIndex < left.legnth && rightIndex < right.length) {
+    if (left[leftIndex] < right[rightIndex]) {
+      result.push(left[leftIndex]);
+      leftIndex++;
+    } else {
+      result.push(right[rightIndex]);
+      rightIndex++;
     }
+  }
+}
+
+function solution4(order) {
+  var answer = 0;
+  for (let i = 0; i < order.length; i++) {
+    if (order[i] == 'americano' || order[i] == 'iceamericano' || order[i] == 'americanoice' || order[i] == 'hotamericano' || order[i] == 'americanohot') {
+      answer += 4500;
+    } else if (order[i] == 'cafelatte' || order[i] == 'icecafelatte' || order[i] == 'cafelatteice' || order[i] == 'hotcafelatte' || order[i] == 'cafelattehot') {
+      answer += 5000;
+    } else {
+      answer += 4500;
+    }
+    //console.log(answer);
   }
   return answer;
 }
 
-console.log(solution4([1, 2, 1, 4, 5, 2, 9]));
+//console.log(solution4(["cafelatte", "americanoice", "hotcafelatte", "anything"]));
+
+
+function solution6(arr, queries) {
+  var answer = [];
+  let temp = [];
+  for (let i = 0; i < queries.length; i++) {
+    for (let j = 1; j < queries[i].length; j++) {
+      let prev = queries[i][j - 1];
+      let next = queries[i][j];
+      temp.push(arr[prev]);
+      arr[prev] = arr[next];
+      arr[next] = temp[0];
+      answer = arr;
+    }
+    temp = [];
+  }
+  return answer;
+}
+
+console.log(solution6([0, 1, 2, 3, 4], [[0, 3], [1, 2], [1, 4]]));
+//[0, 1, 2, 3, 4]
+
+//[[0, 3],
+// [1, 2],
+// [1, 4]]
+
+//[3, 4, 1, 0, 2]
