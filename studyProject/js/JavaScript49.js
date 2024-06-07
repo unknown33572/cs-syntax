15bd447015e4c39c064ae645d58f233fbd9c69ae
dishin <dishin@maitzsoft.co.kr>
2024-06-07 09:07:52 +0900

    코딩연습

---
+++ studyProject/js/JavaScript49.js
@@ -0,0 +1,34 @@
+﻿function solution(arr) {
+  var stk = [];
+  var i = 0;
+  while (i < arr.length) {
+    if (stk.length === 0 || stk[stk.length - 1] < arr[i]) {
+      stk.push(arr[i]);
+      i++;
+    }
+    if (stk[stk.length - 1] >= arr[i]) {
+      stk.pop();
+    }
+  }
+    return stk;
+}
+
+//[1,4,2,5,3]
+console.log(solution([1, 4, 2, 5, 3]));
+
+function solution2(arr) {
+  var stk = [];
+  var i = 0;
+  while (i < arr.length) {
+    if (stk.length === 0 || stk[stk.length - 1] != arr[i]) {
+      stk.push(arr[i]);
+      i++;
+    } else if (stk[stk.length - 1] == arr[i]) {
+      stk.pop();
+      i++;
+    }
+  }
+  if (stk.length === 0) 
+    return stk = [-1];
+    return stk;
+}
