72491893efb8c4f8311f436b44cf44c6d752a9f6
dishin <dishin@maitzsoft.co.kr>
2024-06-14 17:57:57 +0900

    CodingTest

--- studyProject/js/JavaScript50.js
+++ studyProject/js/JavaScript50.js
@@ -46,4 +46,62 @@ function solution2(str_list) {
   return answer;
 }
 
-console.log(solution2(['r','r','r','r']));
+console.log(solution2(['r', 'r', 'r', 'r']));
+
+
+function solution3(arr, queries) {
+  var answer = [];
+  for (var i = 0; i < queries.length; i++) {
+    var oneQuery = queries[i][0];
+    var twoQuery = queries[i][1];
+    var threeQuery = queries[i][2];
+    var temp = [];
+    for (var j = oneQuery; j <= twoQuery; j++) {
+      if (arr[j] > threeQuery) {
+        temp.push(arr[j]);
+      }
+    }
+    if (temp.length > 0) {
+      answer.push(Math.min(...temp));
+    } else {
+      answer.push(-1);
+    }
+  }
+  return answer;
+}
+
+console.log(solution3([0, 1, 2, 4, 3], [[0, 4, 2], [0, 3, 2], [0, 2, 2]]));
+
+console.log(solution3([7, 8, 9], [[0, 2, 10], [0, 2, 3]]));
+
+function solution4(picture, k) {
+  var answer = [];
+  var temp = [];
+
+
+  for (var i = 0; i < picture.length; i++) {
+    var str = picture[i];
+    var finalArr = [];
+    for (var j = 0; j < picture[i].length; j++) {
+      temp = str.split('');
+    }
+    for (var n = 0; n < temp.length; n++) {
+      //for (var m = 0; m < k; m++) {
+      //  answer.push(temp[n]);
+      //}
+      var widthCount = k;
+      while (widthCount > 0) {
+        finalArr.push(temp[n]);
+        widthCount--;
+      }
+    }
+    var heightCount = k;
+    while (heightCount > 0) {
+      answer.push(finalArr.join(''));
+      heightCount--;
+    }
+  }
+  return answer;
+}
+
+console.log(solution4([".xx...xx.", "x..x.x..x", "x...x...x", ".x.....x.", "..x...x..", "...x.x...", "....x...."], 2));
