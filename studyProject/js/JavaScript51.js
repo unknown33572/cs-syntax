8434868a6b6408a65b9037f93a61fe32fef13337
dishin <dishin@maitzsoft.co.kr>
2024-07-02 17:54:54 +0900

    coding test

---
+++ studyProject/js/JavaScript51.js
@@ -0,0 +1,53 @@
+﻿const fs = require('fs');
+const path = require('path');
+
+function BankTransactionAnalyzerSimple(filePath) {
+	try {
+		const data = fs.readFileSync(filePath, 'utf8');
+		const lines = data.split('\n');
+		let total = 0;
+
+		lines.forEach(line => {
+			const columns = line.split(',');
+
+			if (columns[1]) {
+				const amount = parseFloat(columns[1]);
+				total += amount;
+			}
+		});
+		console.log(total);
+	} catch (err) {
+		console.error(err);
+	}
+}
+
+const filePath = path.join(__dirname, 'Banking.csv');
+
+BankTransactionAnalyzerSimple(filePath);
+
+function BankTransactionAnalyzerExtend(filePath) {
+	try {
+		const data = fs.readFileSync(filePath, 'utf8');
+		const lines = data.split('\n');
+		let total = 0;
+
+		lines.forEach(line => {
+			const columns = line.split(',');
+
+			if (columns[0] && columns[1]) {
+				const dateParts = columns[0].split('-');
+				const date = new Date(dateParts[2], dateParts[1] - 1, dateParts[0]);
+
+				if (date.getMonth() === 0) {
+					const amount = parseFloat(columns[1]);
+					total += amount;
+				}
+			}
+		});
+		console.log(total);
+	} catch (err) {
+		console.error(err);
+	}
+}
+
+BankTransactionAnalyzerExtend(filePath);
---
+++ studyProject/js/JavaScript52.js
@@ -0,0 +1,43 @@
+﻿function solution(rank, attendance) {
+  var answer = 0;
+
+  var map = new Map();
+
+  for (var i = 0; i < rank.length; i++) {
+    map.set(rank[i], attendance[i]);
+  }
+
+  var firstIdx = 0;
+  var secondIdx = 0;
+  var thirdIdx = 0;
+  var firstSmallest = Infinity;
+  var secondSmallest = Infinity;
+  var thirdSmallest = Infinity;
+
+  for (let i = 0; i < rank.length; i++) {
+    if (map.get(rank[i]) && rank[i] < firstSmallest) {
+      firstSmallest = rank[i];
+      firstIdx = i;
+    }
+  }
+
+  for (let i = 0; i < rank.length; i++) {
+    if (map.get(rank[i]) && rank[i] < secondSmallest && rank[i] != firstSmallest) {
+      secondSmallest = rank[i];
+      secondIdx = i;
+    }
+  }
+
+  for (let i = 0; i < rank.length; i++) {
+    if (map.get(rank[i]) && rank[i] < thirdSmallest && rank[i] != secondSmallest && rank[i] != firstSmallest) {
+      thirdSmallest = rank[i];
+      thirdIdx = i;
+    }
+  }
+
+  answer = 10000 * firstIdx + 100 * secondIdx + thirdIdx;
+
+  return answer;
+}
+
+solution([1, 2, 3], [true, true, true]);
